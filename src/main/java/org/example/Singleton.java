20c0ab7b4050b5670d4ffa80bc85f6f593f8fcc5
JasonRaza <jason_raza@hotmail.com>
2023-07-03 09:46:46 -0400

    Classe avec singleton

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class Singleton {
 
+    // static = variable de la classe = Class.var
+    // non static = variable d'instance = this.var
+    private static Singleton instance;
+    public Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (Singleton.instance == null)
+            Singleton.instance = new Singleton();
+        return instance;
+    }
 }
